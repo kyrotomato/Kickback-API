ce75e58c90fb3bc39e14df59198d48c997239346
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-06-11 08:43:53 -0500

    installed dependencies and added date format

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,6 @@
 // Require
 const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 // schema for reactions
 const ReactionsSchema = new Schema(
@@ -42,7 +43,7 @@ const ThoughtsSchema = new Schema(
     createdAt: {
         type: Date,
         default: Date.now,
-        get: (createdAt) => dateFormat(createdAt)
+        get: (createdAtVal) => dateFormat(createdAtVal)
     },
     username: {
         type: String,
